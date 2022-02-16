479d319cfaf853a6dcc9c1f7f8e9ba21568a4f1c
Uttarayan Mondal <email@uttarayan.me>
2022-02-16 18:09:06 +0530

    Add full tensorflow repo and build tensorflow with cmake

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use std::{
     env,
     path::{Path, PathBuf},
+    // process::Command,
 };
 
 extern crate bindgen;
@@ -9,8 +10,56 @@ fn out_dir() -> PathBuf {
     PathBuf::from(env::var("OUT_DIR").unwrap())
 }
 
+fn build_tflite_c<P: AsRef<Path>>(tf_src_path: P) -> PathBuf {
+    cmake::Config::new(tf_src_path)
+        .define("TFLITE_C_BUILD_SHARED_LIBS", "OFF")
+        // .define("CMAKE_OSX_ARCHITECTURES", "arm64")
+        .build()
+}
+
+fn link_libs_c<P: AsRef<Path>>(target_dir: P) {
+    let build_dir = target_dir.as_ref().join("build");
+    let search_paths = vec![
+        "ruy-build",
+        "cpuinfo-build",
+        "ff2d-build",
+        "xnnpack-build",
+        "farmhash-build",
+        "clog-build",
+        "flatbuffers-build",
+    ];
+
+    for p in search_paths {
+        println!(
+            "cargo:rustc-link-search=native={}",
+            build_dir.join("_deps").join(p).display()
+        );
+    }
+    println!(
+        "cargo:rustc-link-search=native={}",
+        build_dir.join("pthreadpool").display()
+    );
+    println!("cargo:rustc-link-search=native={}", build_dir.display());
+
+    // println!("cargo:rustc-link-search=native={}", build_dir.join("ruy-build").display());
+    // println!("cargo:rustc-link-search=native={}", build_dir.display());
+}
+
 fn main() {
-    println!("cargo:rustc-link-lib=tensorflowlite_c");
+    println!("cargo:rustc-link-lib=static=farmhash");
+    println!("cargo:rustc-link-lib=static=ruy");
+    println!("cargo:rustc-link-lib=static=cpuinfo");
+    println!("cargo:rustc-link-lib=static=XNNPACK");
+    println!("cargo:rustc-link-lib=static=tensorflow-lite");
+    println!("cargo:rustc-link-lib=static=pthreadpool");
+    println!("cargo:rustc-link-lib=static=fft2d_fftsg");
+    println!("cargo:rustc-link-lib=static=flatbuffers");
+    println!("cargo:rustc-link-lib=static=clog");
+    println!("cargo:rustc-link-lib=static=fft2d_fftsg2d"); // println!("cargo:rustc-link-lib=tensorflowlite_c");
+
+    // println!("cargo:rustc-link-lib=static=tensorflowlite_c");
+    link_libs_c(build_tflite_c("tensorflow/tensorflow/lite/c"));
+    // panic!();
     let out_path = out_dir();
     let tf_src_path = Path::new("./tensorflow");
     let mut builder = bindgen::Builder::default().header(
